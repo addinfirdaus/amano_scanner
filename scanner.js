e21894093f7ffe030d82708c1be420873b5ce90d
Addin Firdaus Yudiarta <addinfirdaus@gmail.com>
2026-06-19 10:39:49 +0700

    Update WEBAPP_URL and change response handling

--- scanner.js
+++ scanner.js
@@ -1,9 +1,9 @@
 const WEBAPP_URL =
-"https://script.google.com/a/macros/siantarmaju.com/s/AKfycbyJgeBI1OkH-aKmuntJ4K_bD_4XmSttHtg32J7KmRuwpAFTmegGNDq4T5tMT7dXdfI_/exec";
+"https://script.google.com/macros/s/AKfycbyJgeBI1OkH-aKmuntJ4K_bD_4XmSttHtg32J7KmRuwpAFTmegGNDq4T5tMT7dXdfI_/exec";
 
 const nik =
 new URLSearchParams(
-window.location.search
+  window.location.search
 ).get("nik");
 
 function onScanSuccess(decodedText){
@@ -24,46 +24,106 @@ async function saveBarcode(kode){
 
   try{
 
-    const response = await fetch(
-        "WEB_APP_URL",
+    const response =
+      await fetch(
+        WEBAPP_URL,
         {
           method:"POST",
-          mode:"no-cors",
-
           headers:{
-            "Content-Type":
-            "application/json"
+            "Content-Type":"application/json"
           },
-
           body:JSON.stringify({
-
             action:"SAVE_BARCODE",
-
             nik:nik,
-
             kodeLokasi:kode
-
           })
-
         }
       );
 
-    const hasil =
-      await response.json();
+    const text =
+      await response.text();
 
-    if(hasil.status){
+    console.log(text);
 
-      alert(
-        "Barcode berhasil disimpan"
-      );
-
-    }
+    alert(text);
 
   }
   catch(err){
 
-    alert(err);
+    alert(
+      err.toString()
+    );
 
   }
 
 }
+
+// const WEBAPP_URL =
+// "https://script.google.com/a/macros/siantarmaju.com/s/AKfycbyJgeBI1OkH-aKmuntJ4K_bD_4XmSttHtg32J7KmRuwpAFTmegGNDq4T5tMT7dXdfI_/exec";
+
+// const nik =
+// new URLSearchParams(
+// window.location.search
+// ).get("nik");
+
+// function onScanSuccess(decodedText){
+
+//   saveBarcode(decodedText);
+
+// }
+
+// new Html5QrcodeScanner(
+//   "reader",
+//   {
+//     fps:10,
+//     qrbox:250
+//   }
+// ).render(onScanSuccess);
+
+// async function saveBarcode(kode){
+
+//   try{
+
+//     const response = await fetch(
+//         "WEB_APP_URL",
+//         {
+//           method:"POST",
+//           mode:"no-cors",
+
+//           headers:{
+//             "Content-Type":
+//             "application/json"
+//           },
+
+//           body:JSON.stringify({
+
+//             action:"SAVE_BARCODE",
+
+//             nik:nik,
+
+//             kodeLokasi:kode
+
+//           })
+
+//         }
+//       );
+
+//     const hasil =
+//       await response.json();
+
+//     if(hasil.status){
+
+//       alert(
+//         "Barcode berhasil disimpan"
+//       );
+
+//     }
+
+//   }
+//   catch(err){
+
+//     alert(err);
+
+//   }
+
+// }
